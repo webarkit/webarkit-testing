4c7d11067c22e018c66d20aa8aec38297b8acf41
kawalt <github@kalwaltart.it>
2020-11-24 17:19:33 +0100

    added WebARKitController

---
+++ src/WebARKitController.js
@@ -0,0 +1,50 @@
+import WebARKit from './WebARKit'
+
+export default class WebARKitController {
+  constructor(){
+    this.listeners = {}
+  }
+  static async init () {
+    // directly init with given width / height
+    const webARC = new WebARKitController()
+    return await webARC._initialize()
+  }
+
+  async _initialize () {
+    // initialize the toolkit
+    this.webarkit = await new WebARKit().init()
+    console.log('[WebARController]', 'WebARKit initialized')
+
+    setTimeout(() => {
+      this.dispatchEvent({
+        name: 'load',
+        target: this
+      })
+    }, 1)
+      return this
+  }
+  addEventListener(name, callback) {
+    if(!this.listeners[name]) {
+      this.listeners[name] = [];
+    }
+    this.listeners[name].push(callback);
+  };
+
+  removeEventListener(name, callback) {
+    if(this.listeners[name]) {
+      let index = this.listeners[name].indexOf(callback);
+      if(index > -1) {
+        this.listeners[name].splice(index, 1);
+      }
+    }
+  };
+
+  dispatchEvent(event) {
+    let listeners = this.listeners[event.name];
+    if(listeners) {
+      for(let i = 0; i < listeners.length; i++) {
+        listeners[i].call(this, event);
+      }
+    }
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import WebARKit from './WebARKit'
+import WebARKitController from './WebARKitController'
 
 export default {
-  WebARKit
+  WebARKit, WebARKitController
 }
