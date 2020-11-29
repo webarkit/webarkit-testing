6bc171a1cd344ce00f03dba991a1e134591d5782
kalwalt <github@kalwaltart.it>
2020-11-29 18:24:31 +0100

    allow_raw_pointers() is not needed

--- emscripten/bindings.cpp
+++ emscripten/bindings.cpp
@@ -5,5 +5,5 @@ using namespace emscripten;
 EMSCRIPTEN_BINDINGS(constant_bindings) {
     function("test", &test);
     function("setup", &setup);
-    function("initTracking", &initTracking, allow_raw_pointers());
+    function("initTracking", &initTracking);
 }
