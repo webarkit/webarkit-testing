11d7dd28cefdfc252287cf507457c9b3c25a612a
walter perdan <info@kalwaltart.it>
2020-12-06 20:03:50 +0100

    partially works with typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
   output: {
     path: path.resolve('dist'),
     filename: 'WebARKit.js',
@@ -12,29 +25,8 @@ module.exports = {
     // @see: https://github.com/webpack/webpack/issues/6522
     globalObject: 'typeof self !== \'undefined\' ? self : this',
   },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              // @see https://github.com/babel/babel/issues/9849
-              ['@babel/transform-runtime']
-            ]
-          }
-        }]
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js']
-  },
-  // @see https://stackoverflow.com/questions/59487224/webpack-throws-error-with-emscripten-cant-resolve-fs
   node: {
     'fs': 'empty'
   }
 };
+
