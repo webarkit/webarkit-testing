6b188ee48a7542330aeed2cf5f017bfaff7f49a7
Walter Perdan <github@kalwaltart.it>
2023-04-21 17:35:10 +0200

    debug symbols to try to solve issue while tracking

--- tools/makem.js
+++ tools/makem.js
@@ -174,11 +174,11 @@ FLAGS += ' -s USE_LIBJPEG';
 FLAGS += ' --memory-init-file 0 '; // for memless file
 FLAGS += ' -s "EXPORTED_RUNTIME_METHODS=[\'FS\']"';
 FLAGS += ' -s ALLOW_MEMORY_GROWTH=1';
-FLAGS += ' -s DISABLE_EXCEPTION_CATCHING=0 ';
-//FLAGS += ' -gsource-map -fsanitize=address '
-//FLAGS += ' -s ASSERTIONS=2 '
-//FLAGS += '  -s DEMANGLE_SUPPORT=1 '; 
-FLAGS += ' --profiling '
+//FLAGS += ' -s DISABLE_EXCEPTION_CATCHING=0 ';
+FLAGS += ' -gsource-map -fsanitize=address '
+FLAGS += ' -s ASSERTIONS=2 '
+FLAGS += '  -s DEMANGLE_SUPPORT=1 '; 
+//FLAGS += ' --profiling '
 // FLAGS += ' -s SAFE_HEAP=1 '
 
 var WASM_FLAGS = ' -s SINGLE_FILE=1 '
