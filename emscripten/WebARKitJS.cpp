9cc0ee222c09ba5e9e93db7c5e67d0eb3e103e0d
Walter Perdan <github@kalwaltart.it>
2023-05-01 23:52:40 +0200

    simplifying C++ code - removing not necessary classes - fix issue #11

--- emscripten/WebARKitJS.cpp
+++ emscripten/WebARKitJS.cpp
@@ -3,38 +3,19 @@
 void WebARKit::initTrackerGray(emscripten::val data_buffer, int width, int height) {
   auto u8 =
       emscripten::convertJSArrayToNumberVector<uint8_t>(data_buffer);
-  if (this->m_trackerType == webarkit::TRACKER_TYPE::AKAZE_TRACKER) {
-    m_akaze_tracker->initialize_gray_raw(u8.data(), width, height);
-  } else if (this->m_trackerType == webarkit::TRACKER_TYPE::ORB_TRACKER) {
-    m_orb_tracker->initialize_gray_raw(u8.data(), width, height);
-  } else {
-    throw std::invalid_argument("Invalid tracker type");
-  }
+  m_tracker->initTracker(u8.data(), width, height);
 }
 
 void WebARKit::processFrame(emscripten::val data_buffer, webarkit::ColorSpace colorSpace) {
   auto u8 =
       emscripten::convertJSArrayToNumberVector<uint8_t>(data_buffer);
-  if (this->m_trackerType == webarkit::TRACKER_TYPE::AKAZE_TRACKER) {
-    m_akaze_tracker->processFrameData(u8.data(), this->videoWidth,
+  m_tracker->processFrameData(u8.data(), this->videoWidth,
                                       this->videoHeight, colorSpace);
-  } else if (this->m_trackerType == webarkit::TRACKER_TYPE::ORB_TRACKER) {
-    m_orb_tracker->processFrameData(u8.data(), this->videoWidth,
-                                    this->videoHeight, colorSpace);
-  } else {
-    throw std::invalid_argument("Invalid tracker type");
-  }
 }
 
 emscripten::val WebARKit::getHomography() {
   std::vector<double> output;
-  if (this->m_trackerType == webarkit::TRACKER_TYPE::AKAZE_TRACKER) {
-    output = m_akaze_tracker->getOutputData();
-  } else if (this->m_trackerType == webarkit::TRACKER_TYPE::ORB_TRACKER) {
-    output = m_orb_tracker->getOutputData();
-  } else {
-    throw std::invalid_argument("Invalid tracker type");
-  }
+   output = m_tracker->getOutputData();
   emscripten::val homography = emscripten::val::array();
   for (auto i = 0; i < 9; i++) {
     homography.call<void>("push", output[i]);
@@ -45,13 +26,7 @@ emscripten::val WebARKit::getHomography() {
 
 emscripten::val WebARKit::getCorners() {
    std::vector<double> output;
-  if (this->m_trackerType == webarkit::TRACKER_TYPE::AKAZE_TRACKER) {
-    output = m_akaze_tracker->getOutputData();
-  } else if (this->m_trackerType == webarkit::TRACKER_TYPE::ORB_TRACKER) {
-    output = m_orb_tracker->getOutputData();
-  } else {
-    throw std::invalid_argument("Invalid tracker type");
-  }
+  output = m_tracker->getOutputData();
   emscripten::val corners = emscripten::val::array();
   for (auto i = 9; i < 17; i++) {
     corners.call<void>("push", output[i]);
@@ -60,15 +35,7 @@ emscripten::val WebARKit::getCorners() {
 }
 
 bool WebARKit::isValid() {
-  auto valid = false;
-  if (this->m_trackerType == webarkit::TRACKER_TYPE::AKAZE_TRACKER) {
-    valid = m_akaze_tracker->isValid();
-  } else if (this->m_trackerType == webarkit::TRACKER_TYPE::ORB_TRACKER) {
-    valid = m_orb_tracker->isValid();
-  } else {
-    throw std::invalid_argument("Invalid tracker type");
-  }
-  return valid;
+  return m_tracker->isValid();
 }
 
 #include "bindings.cpp"
