9e28ed64b87d05c23794cc06173fb24239ff2bb2
kalwalt <github@kalwaltart.it>
2023-10-24 00:22:48 +0200

    new build commands

--- build_cv_w_docker.sh
+++ build_cv_w_docker.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ ! -d "opencv_js" ] ; then
+    mkdir opencv_js
+    sudo chmod -R 777 opencv_js
+    echo "mkdir opencv_js"
+fi
+
 docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) -e "EMSCRIPTEN=/emsdk/upstream/emscripten"  emscripten/emsdk:3.1.26 emcmake python3 ./opencv/platforms/js/build_js.py opencv_js --config="./opencv.webarkit_config.py" --build_wasm --cmake_option="-DBUILD_opencv_dnn=OFF"  --cmake_option="-DBUILD_opencv_objdetect=OFF" --cmake_option="-DBUILD_opencv_photo=OFF" --cmake_option="-DBUILD_opencv_imgcodecs=ON" --cmake_option="-DBUILD_opencv_xfeatures2d=ON"  --cmake_option="-DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules/" --build_flags="-Oz -s EXPORT_ES6=1 -s USE_ES6_IMPORT_META=0"
 
 # copy the output to the build folder
