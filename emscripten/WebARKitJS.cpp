39df258330bc7e74750017a1900339d256e13a64
kalwalt <github@kalwaltart.it>
2023-11-04 17:07:28 +0100

    the wrong camera matrix was causing the issue - WebARKitCamera need to be changed

--- emscripten/WebARKitJS.cpp
+++ emscripten/WebARKitJS.cpp
@@ -24,10 +24,11 @@ emscripten::val WebARKit::getHomography() {
 
 emscripten::val WebARKit::getPoseMatrix() {
     cv::Mat poseMatrix = manager.getPoseMatrix();
+    WEBARKIT_LOGi("Mat shape: %d x %d", poseMatrix.rows, poseMatrix.cols);
     emscripten::val pose = emscripten::val::array();
-    for (auto i = 0; i < 3; i++) {
-        for (auto j = 0; j < 4; j++) {
-            pose.call<void>("push", poseMatrix.at<double>(j, i));
+    for (auto i = 0; i < poseMatrix.rows; i++) {
+        for (auto j = 0; j < poseMatrix.cols; j++) {
+            pose.call<void>("push", poseMatrix.at<double>(i, j));
         }
     }
     return pose;
