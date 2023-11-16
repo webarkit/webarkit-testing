993fcbfbd25d6f085a5825d289149605a38add8d
Walter Perdan <github@kalwaltart.it>
2023-11-16 23:36:10 +0100

    missed files for testing matrix and cameraProjmatrix - continue from previous commit

---
+++ examples/worker_threejs.js
@@ -0,0 +1,76 @@
+importScripts("../dist/WebARKit.js");
+
+var ar;
+var next = null;
+var markerResult = null;
+
+self.onmessage = (e) => {
+  var msg = e.data;
+  switch (msg.type) {
+    case "initTracker": {
+      initTracker(msg);
+      return;
+    }
+    case "process": {
+      next = msg.data;
+      processFrame();
+      return;
+    }
+  }
+};
+
+function initTracker(msg) {
+  var trackerType = msg.trackerType;
+  //console.log("msg from initTracker: ", msg);
+
+  var onLoad = function (wark) {
+    ar = wark;
+    //console.log("wark: ", wark)
+    wark.loadTrackerGrayImage(msg.imageData, msg.imgWidth, msg.imgHeight, WebARKit.WebARKitController.RGB);
+
+    var cameraProjMat = wark.getCameraProjectionMatrix();
+    console.log("camera proj Mat: ", cameraProjMat);
+
+    postMessage({
+      type: "loadedTracker",
+      cameraProjMat: JSON.stringify(cameraProjMat),
+    })
+
+    postMessage({ type: "endLoading", end: true })
+
+    wark.addEventListener("getMarker", function (event) {
+      console.log(event.data.corners);
+      markerResult = {
+        type: "found",
+        corners: JSON.stringify(event.data.corners),
+        matrix: JSON.stringify(event.data.matrix),
+        matrixGL_RH: JSON.stringify(event.data.matrixGL_RH),
+        pose: JSON.stringify(event.data.pose),
+      };
+    });
+  };
+
+  var onError = function (error) {
+    console.error(error);
+  };
+
+  WebARKit.WebARKitController.init_raw(msg.videoWidth, msg.videoHeight, trackerType)
+    .then(onLoad)
+    .catch(onError);
+}
+
+function processFrame() {
+  markerResult = null;
+  if (ar && ar.process_raw) {
+    ar.process_raw(next.data, WebARKit.WebARKitController.RGBA)
+  }
+  if (markerResult) {
+    postMessage(markerResult);
+  } else {
+    postMessage({
+      type: "not found",
+    });
+  }
+
+  next = null;
+}
